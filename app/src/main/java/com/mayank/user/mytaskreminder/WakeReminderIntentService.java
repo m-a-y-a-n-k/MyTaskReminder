f394c6835120c03cca0a195dfb2321db50680aff
user <mayanknarula96@gmail.com>
2016-07-07 18:59:15 +0530

    Wake Lock Manager Intent Service Commit

--- app/src/main/java/com/mayank/user/mytaskreminder/WakeReminderIntentService.java
+++ app/src/main/java/com/mayank/user/mytaskreminder/WakeReminderIntentService.java
@@ -10,15 +10,16 @@ import android.content.Intent;
 import android.os.PowerManager;
 
 public abstract class WakeReminderIntentService extends IntentService {
-    abstract void doReminderWork(Intent intent);
+    abstract void doReminderWork(Intent intent);                                                 // Implementation in ReminderService class
 
-    public static final String LOCK_NAME_STATIC="com.dummies.android.taskreminder.Static";
+    public static final String LOCK_NAME_STATIC="com.mayank.user.mytaskreminder.Static";        // STATIC_LOCK_NAME
     private static PowerManager.WakeLock lockStatic=null;
 
     public static void acquireStaticLock(Context context) {
         getLock(context).acquire();
     }
 
+    // synchronized method to get System Service WakeLock from Power Manager
     synchronized private static PowerManager.WakeLock getLock(Context context) {
         if (lockStatic==null) {
             PowerManager mgr=(PowerManager)context.getSystemService(Context.POWER_SERVICE);
@@ -33,6 +34,7 @@ public abstract class WakeReminderIntentService extends IntentService {
         super(name);
     }
 
+    // When Service is doing Reminder Work , this method is waiting to finish the last step of lock release from Power Manager
     @Override
     final protected void onHandleIntent(Intent intent) {
         try {
