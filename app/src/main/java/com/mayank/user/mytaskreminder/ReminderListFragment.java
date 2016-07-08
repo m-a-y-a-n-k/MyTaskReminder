bc71927af2fc7f0ba017bbad459b33ce0a6df3e4
user <mayanknarula96@gmail.com>
2016-07-08 21:06:20 +0530

    gradle dependencies commit\

---
+++ app/src/main/java/com/mayank/user/mytaskreminder/ReminderListFragment.java
@@ -0,0 +1,113 @@
+package com.mayank.user.mytaskreminder;
+
+import android.app.ListFragment;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+
+/**
+ * Created by user on 08-07-2016.
+ */
+public class ReminderListFragment extends ListFragment {
+
+    private static final int ACTIVITY_EDIT=1;
+    private RemindersDbAdapter mDbHelper;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    public ReminderListFragment() {
+        super();
+        mDbHelper = new RemindersDbAdapter(getActivity());
+        mDbHelper.open();
+        fillData();
+        registerForContextMenu(getListView());
+    }
+
+    private void fillData() {
+        Cursor remindersCursor = mDbHelper.fetchAllReminders();
+        //startManagingCursor(remindersCursor);
+
+        // Create an array to specify the fields we want to display in the list (only TITLE)
+        String[] from = new String[]{RemindersDbAdapter.KEY_TITLE};
+
+        // and an array of the fields we want to bind those fields to (in this case just text1)
+        int[] to = new int[]{R.id.text1};
+
+        // Now create a simple cursor adapter and set it to display
+        SimpleCursorAdapter reminders =
+                new SimpleCursorAdapter(getActivity(), R.layout.reminder_row, remindersCursor, from, to);
+        setListAdapter(reminders);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater mi) {
+        super.onCreateOptionsMenu(menu, mi);
+        mi.inflate(R.menu.list_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater mi = getActivity().getMenuInflater();
+        mi.inflate(R.menu.list_menu_item_longpress, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_delete:
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                mDbHelper.deleteReminder(info.id);
+                fillData();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent i = new Intent(getActivity(), ReminderEditActivity.class);
+        i.putExtra(RemindersDbAdapter.KEY_ROWID, id);
+        startActivityForResult(i, ACTIVITY_EDIT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        fillData();
+    }
+}
